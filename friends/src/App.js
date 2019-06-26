14924a7eeb5277a96564efdb952c6af27c78511a
clay-most <46730958+clay-most@users.noreply.github.com>
2019-06-25 18:10:00 -0600

    start state on app

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import FriendsList from './components/FriendsList';
+import NewFriend from './components/NewFriend';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends React.Component {
-  render() {
-    return (<div className='App'>
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
 
-    </div>)
+  componentDidMount() {
+    axios('http://localhost:5000/friends')
+      .then(res => this.setState({ friends: res.data }))
+      .catch(err => {
+        throw new Error(err);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <FriendsList />
+        <NewFriend />
+      </div>
+    );
   }
 }
 
