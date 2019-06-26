237abc85bd6b78a570d40837a32abad7070a08c1
clay-most <46730958+clay-most@users.noreply.github.com>
2019-06-26 11:19:01 -0600

    more typos

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -4,32 +4,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import FriendCard from './FriendCard';
 
-class FreindsList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        <Link to="/new">
-          <button>New Friend!</button>
-        </Link>
-        <div className="list">
-          {props.friends.map(friend => (
-            <Friend
-              name={friend.name}
-              age={friend.age}
-              email={friend.email}
-              id={friend.id}
-              key={friend.id}
-              delete={props.delete}
-              edit={props.edit}
-            />
-          ))}
-        </div>
-      </div>
-    );
-  }
-}
+const FriendsList = props => {
+  <div>
+    <Link to="/new">
+      <button>New Friend!</button>
+    </Link>
+    <div className="list">
+      {props.friends.map(friend => (
+        <FriendCard
+          name={friend.name}
+          age={friend.age}
+          email={friend.email}
+          id={friend.id}
+          key={friend.id}
+          edit={props.edit}
+          delete={props.delete}
+        />
+      ))}
+    </div>
+  </div>;
+};
 
-export default FreindsList;
+export default FriendsList;
