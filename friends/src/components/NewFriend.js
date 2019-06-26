303ef952d70e645f12480cd9d75d7bebb8672fc1
clay-most <46730958+clay-most@users.noreply.github.com>
2019-06-26 10:47:31 -0600

    more typos

--- friends/src/components/NewFriend.js
+++ friends/src/components/NewFriend.js
@@ -1,10 +1,10 @@
 // new friend form on submit pushes new friend to state//
 import React from 'react';
 
-class NewFreind extends React.Component {
+class NewFriend extends React.Component {
   render() {
     return <div>Form</div>;
   }
 }
 
-export default NewFreind;
+export default NewFriend;
