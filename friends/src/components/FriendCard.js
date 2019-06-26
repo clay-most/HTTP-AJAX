8d176b1ab24de17e802e059490e20c6e70b330e2
clay-most <46730958+clay-most@users.noreply.github.com>
2019-06-26 11:01:26 -0600

    build new friend button

--- friends/src/components/FriendCard.js
+++ friends/src/components/FriendCard.js
@@ -2,6 +2,9 @@
 import React from 'react';
 
 class FriendCard extends React.Component {
+    constructor(props){
+        super(props)
+    }
   render() {
     return <div>Card</div>;
   }
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,12 +1,21 @@
 //feeds Indiviual friends to FriendsCard and renders them//
 import React from 'react';
+
+import { Link } from 'react-router-dom';
 import FriendCard from './FriendCard';
 
 class FreindsList extends React.Component {
+  constructor(props) {
+      super(props)
+  }
   render() {
-    return (<div>List
-        <FriendCard />
-        </div>);
+    return (
+      <div>
+        <Link to="/new">
+          <button>New Friend!</button>
+        </Link>
+      </div>
+    );
   }
 }
 
